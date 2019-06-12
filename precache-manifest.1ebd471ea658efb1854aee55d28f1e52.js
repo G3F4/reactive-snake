0934375484dd8f694e5ffc79fbb6889b068b768c
Kamil Grabek <kamil.grabek@allegro.pl>
2019-06-12 19:13:59 +0200

    Updates

---
+++ precache-manifest.1ebd471ea658efb1854aee55d28f1e52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a28e0eb52b31c93a3d0308a86a87343",
+    "url": "/$npm_package_name/index.html"
+  },
+  {
+    "revision": "8a4d3d2243131ecd5814",
+    "url": "/$npm_package_name/static/css/main.d32af6b1.chunk.css"
+  },
+  {
+    "revision": "3f3339bb7b4ac5764815",
+    "url": "/$npm_package_name/static/js/2.a01e2e1e.chunk.js"
+  },
+  {
+    "revision": "8a4d3d2243131ecd5814",
+    "url": "/$npm_package_name/static/js/main.889c904d.chunk.js"
+  },
+  {
+    "revision": "81108b125df5e6fe78e7",
+    "url": "/$npm_package_name/static/js/runtime~main.0eadd600.js"
+  }
+]);
