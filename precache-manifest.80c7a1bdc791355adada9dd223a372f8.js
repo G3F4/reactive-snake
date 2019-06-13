407dae2e33898d710e4b972d34c5cf39a3ea6f29
Kamil Grabek <kamil.grabek@allegro.pl>
2019-06-13 21:34:38 +0200

    Updates

---
+++ precache-manifest.80c7a1bdc791355adada9dd223a372f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c9bbdf4c1bc3b9700e895d08e5f8d7",
+    "url": "/reactive-snake/index.html"
+  },
+  {
+    "revision": "306583ab947afbb0b938",
+    "url": "/reactive-snake/static/css/main.ad60d84f.chunk.css"
+  },
+  {
+    "revision": "9540927fbd30bbc0bb49",
+    "url": "/reactive-snake/static/js/2.8c18d253.chunk.js"
+  },
+  {
+    "revision": "306583ab947afbb0b938",
+    "url": "/reactive-snake/static/js/main.ff42d924.chunk.js"
+  },
+  {
+    "revision": "7db10b829de7cd88aab9",
+    "url": "/reactive-snake/static/js/runtime~main.7ecd7681.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactive-snake/precache-manifest.027c0e98b9feb69a8affb56e5ac4f35b.js"
+  "/reactive-snake/precache-manifest.80c7a1bdc791355adada9dd223a372f8.js"
 );
 
 self.addEventListener('message', (event) => {
